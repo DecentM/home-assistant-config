9891a294c5ba9153f2836e21f8a0db7bd693d6b8
DecentM <decentm@decentm.com>
2020-02-29 22:52:12 +0200

    fix: cd to /config before fetching from git

--- shell_scripts/git-pull.sh
+++ shell_scripts/git-pull.sh
@@ -5,6 +5,8 @@ set -e
 
 DATE=$(date --rfc-3339=seconds)
 
+cd /config
+
 # Prevent conflicts
 git stash save -u "automated pull on $DATE"
 # Fetch and reset instead of pull in case the branch was force-pushed
