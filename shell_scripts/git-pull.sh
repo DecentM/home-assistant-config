b025aa7b8277924857dafea479d2f14426994549
DecentM <decentm@decentm.com>
2020-02-29 22:45:07 +0200

    feat: add automatic git pull on webhook

---
+++ shell_scripts/git-pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Quit on the first error
+set -e
+
+DATE=$(date --rfc-3339=seconds)
+
+# Prevent conflicts
+git stash save -u "automated pull on $DATE"
+# Fetch and reset instead of pull in case the branch was force-pushed
+git fetch
+git reset --hard origin/master
