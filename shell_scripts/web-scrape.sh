685ab07c78f17058623a138b887fa8914a1ee746
DecentM <decentm@decentm.com>
2020-03-05 23:15:19 +0200

    feat: add web_scrape shell script

---
+++ shell_scripts/web-scrape.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Do not quit immediately if an error occurs
+set +e
+
+# Uh I'm totally not parsing your website, nope!
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36"
+
+scrape () {
+  local URL
+  URL="$1"
+
+  local QUERY
+  QUERY="$2"
+
+  if [ -z "$URL" ] || [ "$URL" == "help" ] || [ "$URL" == "--help" ] || [ -z "$QUERY" ]; then
+    printf "Usage: %s %s %s %s\n\n" "$0" "<URL>" "<query>" "[debug]"
+    printf "\tURL must be an accessible URL that return HTML.\n"
+    printf "\tQuery must be a pup query that return text.\n"
+    exit 1
+  fi
+
+  local HTML
+  HTML=$(curl -s -A "$USER_AGENT" "$URL")
+
+  local RESULT
+  RESULT=$(printf "%s\n" "$HTML" | pup "$QUERY")
+
+  printf "%s\n" "$RESULT"
+}
+
+scrape "$1" "$2"
